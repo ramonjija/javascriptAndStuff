5dc147bc29f51c68ffdae927c56814581f53f7b4
Ramon Silva <ramon.silva@al.infnet.edu.br>
2015-04-30 15:54:44 -0300

    Adição da logica de exibição e classes de erro e acerto

--- DNA/JS/application.js
+++ DNA/JS/application.js
@@ -1,24 +1,42 @@
 var main = function(){
 
-	var matrizBasesNitrogenadas = ['a','g','c','t'];
+	var matrizBasesNitrogenadas = ['A','G','T','C'];
 	for(i = 0; i < matrizBasesNitrogenadas.length; i++){
 		$(".basesNitrogenadas").append("<option value="+matrizBasesNitrogenadas[i]+">"+matrizBasesNitrogenadas[i]+"</option>");
 	}
 
-	//var base3 = document.getElementById('');
-	//var base5;
-/*
-	var ligacoesDNA = function(base5, base3){
+	function ligacoesDNA(base5, base3){
 		var result = false;
-		if((base5 == 'a' && base3 == 't') || (base5 == 't' && base3 == 'a'))
+		if((base5 == 'A' && base3 == 'T') || (base5 == 'T' && base3 == 'A'))
 		{
 			result = true;
-		}else if((base5 == 'c' && base3 == 'g')) || (base5 == 'g' && base3 == 'c'))
+		}else if((base5 == 'C' && base3 == 'G') || (base5 == 'G' && base3 == 'C'))
 		{
 			result = true;
 		}
 		return result;
 	}
-*/
+
+	$("#btnAvaliaBases").click(function(){
+		var base3 = $(".baseNitrogenada5 > .basesNitrogenadas option:selected").text();
+		var base5 = $(".baseNitrogenada3 > .basesNitrogenadas option:selected").text();
+
+		if(ligacoesDNA(base5,base3) == true){
+
+			$(".dna5").append("<p class='acertoDeBases'>"+base5+"</p>");
+			$(".dna3").append("<p class='acertoDeBases'>"+base3+"</p>");
+			
+		}else{
+			
+			$(".dna5").append("<p class='erroDeBases'>"+base5+"</p>");
+			$(".dna3").append("<p class='erroDeBases'>"+base5+"</p>");
+			
+			alert("Erro de Bases!");
+		}
+	});
+	
+
+	
+
 }
 $(document).ready(main);
